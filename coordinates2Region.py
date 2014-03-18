4c889d76f9277e2c58fef0121df3b00acce5469a
Travis Collins <tc@172-30-7-141.dynapool.nyu.edu>
2014-03-18 15:29:27 -0400

    Add the code to test

---
+++ coordinates2Region.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+"""This module was created to demonstrate testing at the 03/2014 NYU 
+Software Carpentry Bootcamp.  The functions below contain 
+documentation on their use."""
+
+def coordinates2Region():
+	"""Takes an input coordinate and returns a region"""
+	pass
+
+def main():
+	"""This is the main controller for my module."""
+	print 'Hi!'
+
+if __name__ == '__main__':
+	main()
+	
